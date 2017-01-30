2dcb1fdf6bbcb2c06d69f2754a010039b9240c92
Aman Jain <amannayak15@gmail.com>
2017-01-31 00:14:38 +0530

    Gate Calculator
    YYOOYO

--- Gate Calculator/ViewController.swift
+++ Gate Calculator/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       let url = Bundle.main.url(forResource: "SCIENTIFIC CALCULATOR", withExtension: "htm")
+        let request = NSURLRequest(url: url!)
+        webView.loadRequest(request as URLRequest)
     }
 
     override func didReceiveMemoryWarning() {
